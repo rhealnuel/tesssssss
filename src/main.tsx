459107699bead497ee8422a17a3b1c8d1f1e0da1
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2025-02-26 15:00:04 +0100

    pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./serviceWorker";
+import { registerSW } from "virtual:pwa-register";
+
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,4 +23,3 @@ createRoot(document.getElementById("root")!).render(
 );
 
 // Register the service worker after rendering
-registerServiceWorker();
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+
 
 
 // https://vite.dev/config/
@@ -8,5 +10,25 @@ export default defineConfig({
   plugins: [
     react(),     
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My Vite PWA",
+        short_name: "VitePWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 })
